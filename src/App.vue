3afcc947565e26d9583dfa32cad8f6267978fd16
Sebastian Maciel <sebamaciel15@gmail.com>
2021-10-21 16:22:17 -0300

    carrito usando vue3 composition API

---
+++ src/App.vue
@@ -0,0 +1,45 @@
+<template> 
+<div class="container">
+  <h1>Carrito de compras</h1>
+  <hr>
+  <hr>
+  <div class="row">
+    <Card
+      v-for="producto of productos" :key="producto.id"
+      :producto="producto"
+     />
+  </div>
+  <Carrito/>
+</div>
+  
+</template>
+
+<script>
+import {useStore} from 'vuex'
+import { computed, onMounted } from 'vue';
+import Card from './components/Card.vue';
+import Carrito from './components/Carrito.vue';
+
+export default {
+  name: "App",
+  components: {
+    Card,
+    Carrito
+  },
+  setup(){
+    const store = useStore()
+    onMounted( () => {
+      store.dispatch('fetchData')
+    })
+
+    const productos = computed(() => store.state.productos)
+
+    //const carrito = computed(() => store.state.carrito)
+
+    return{
+      productos,
+    }
+  }
+};
+</script>
+
---
+++ src/components/Card.vue
@@ -0,0 +1,36 @@
+<template id="template-producto">
+    <div class="col-12 col-sm-4 col-md-3 col-lg-2 mb-3 mb-3">
+        <div class="card">
+            <img :src="producto.thumbnailUrl" :alt="`imagen-${producto.title}`" class="card-img-top">
+            <div class="card-body">
+                <h5 class="card-title">{{producto.title}}</h5>
+                <p class="card-text">$ <span>{{producto.precio}}</span></p>
+                <button class="btn btn-dark" @click="agregar(producto)">Comprar</button>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+import {useStore} from 'vuex';
+
+export default {
+    props: ['producto'],
+    setup(){
+
+        const store= useStore()
+
+        const agregar= (producto) => {
+            store.dispatch('agregarAlCarro', producto)
+        }
+
+        return{
+            agregar,
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
